76b91013274b699418b003163fe6597f600c6d20
AparnaSNa <53826817+AparnaSNa@users.noreply.github.com>
2020-12-11 17:17:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(2000,1000);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  triangle(30,75, 58, 20, 86, 75);
+  rect(30,80,55,600)
+  rect(85,300,300,380)
+  rect(350,180,300,500)
+  triangle(350,180,490,20,650,180)
+  rect(650,300,300,380)
+  triangle(950,75, 995, 20, 1050, 75);
+  rect(950,80,95,600)
+
+}
